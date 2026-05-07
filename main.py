224053a203e90de4c9c9f5ab71e139d51a377c27
lasan97 <fktks97@naver.com>
2026-05-07 19:06:45 +0900

    3d 그래프

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sympy import *
+from sympy.plotting import plot3d
 
 def _선형함수():
     x = symbols('x')
@@ -10,6 +11,10 @@ def _2차함수():
     f = x**2 + 1
     plot(f)
 
+def _3차원그래프():
+    x, y = symbols('x y')
+    f = 2*x + 3*y
+    plot3d(f)
 
 if __name__ == '__main__':
-    _2차함수()
+    _3차원그래프()
