2036620a83c462d42b299b11b1c241db69353e3e
lasan97 <fktks97@naver.com>
2026-05-07 18:56:54 +0900

    2차 함수

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from sympy import *
 
-
-
-def f():
+def _선형함수():
     x = symbols('x')
     f = 2*x + 1
     plot(f)
 
+def _2차함수():
+    x = symbols('x')
+    f = x**2 + 1
+    plot(f)
 
 
 if __name__ == '__main__':
-    f()
+    _2차함수()
