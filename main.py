aa0e3964c9dcb0f944d18ac0fbc116739cd9f674
lasan97 <fktks97@naver.com>
2026-05-11 19:57:59 +0900

    극한 기울기 계산

--- main.py
+++ main.py
@@ -102,6 +102,17 @@ def _편도함수():
 
     plot3d(f)
 
+def _극한_기울기():
+    x,s = symbols('x s')
+
+    f = x**2
+    slope_f = (f.subs(x, x + s) - f) / ((x+s) - x)
+
+    slope_2 = slope_f.subs(x, 2)
+
+    result = limit(slope_2, s, 0)
+
+    print(result)
 
 if __name__ == '__main__':
-    _편도함수()
+    _극한_기울기()
