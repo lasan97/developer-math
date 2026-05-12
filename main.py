536a2208c2469e7db5332996cec58aefbdfffff7
lasan97 <fktks97@naver.com>
2026-05-13 01:08:24 +0900

    적분 근사

--- main.py
+++ main.py
@@ -154,6 +154,32 @@ def _연쇄법칙_유무와_상관없이_dz_dx_계산결과는_같음():
     print(dz_dx_chain)
     print(dz_dx_no_chain)
 
+def _적분_근사():
+
+    def approximate_integral(a, b, n, f):
+        delta_x = (b - a) / n
+        total_sum = 0
+
+        for i in range(1, n + 1):
+            midpoint = 0.5 * (2 * a + delta_x * (2 * i - 1))
+            total_sum += f(midpoint)
+
+        return total_sum * delta_x
+
+    def my_function(x):
+        return x**2 + 1
+
+    area = approximate_integral(a=0, b=1, n=5, f=my_function)
+    print(area)
+
+    area = approximate_integral(a=0, b=1, n=500, f=my_function)
+    print(area)
+
+    area = approximate_integral(a=0, b=1, n=1000, f=my_function)
+    print(area)
+
+    area = approximate_integral(a=0, b=1, n=1000000, f=my_function)
+    print(area)
+
 if __name__ == '__main__':
-    # _x에대한_z도함수()
-    _연쇄법칙_유무와_상관없이_dz_dx_계산결과는_같음()
+    _적분_근사()
